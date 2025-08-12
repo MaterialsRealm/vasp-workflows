0e9a7adb59cfee28f21421320f2efa8dc04bed3d
singularitti <singularitti@outlook.com>
2025-08-12 18:45:09 -0500

    Export `PoscarContcarMover`

--- src/vasp_wfl/poscar.py
+++ src/vasp_wfl/poscar.py
@@ -14,6 +14,7 @@ from .dirs import WorkdirFinder
 __all__ = [
     "ElementCounter",
     "ElementExtractor",
+    "PoscarContcarMover",
     "SiteExtractor",
     "StructureParser",
     "SymmetryDetector",
