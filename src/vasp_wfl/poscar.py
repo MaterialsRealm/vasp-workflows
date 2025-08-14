85862504fc49c377ba9f7792ddd4cf2ea1c3130d
singularitti <singularitti@outlook.com>
2025-08-14 04:13:27 -0500

    Fix `cif_to_poscar`

--- src/vasp_wfl/poscar.py
+++ src/vasp_wfl/poscar.py
@@ -185,11 +185,8 @@ class SymmetryDetector(StructureProcessor):
 def cif_to_poscar(cif_files):
     for cif_file in cif_files:
         cif_dir = os.path.dirname(os.path.abspath(cif_file))
-        structure_name = os.path.splitext(os.path.basename(cif_file))[0]
-        out_dir = os.path.join(cif_dir, structure_name)
-        os.makedirs(out_dir, exist_ok=True)
+        poscar_path = os.path.join(cif_dir, "POSCAR")
         atoms = read(cif_file)
-        poscar_path = os.path.join(out_dir, "POSCAR")
         write(poscar_path, atoms, format="vasp")
 
 
