825d95957a9bdddf8b2ea1e43fa0bf109219f7eb
singularitti <singularitti@outlook.com>
2025-09-04 17:52:20 -0500

    Add `cell_to_input` in src/vasp_wfl/collinear.py

--- src/vasp_wfl/collinear.py
+++ src/vasp_wfl/collinear.py
@@ -1,9 +1,11 @@
-from pymatgen.io.vasp import Incar
+from pathlib import Path
+
+from pymatgen.io.vasp import Incar, Poscar
 
 from .poscar import AtomsExtractor, LatticeExtractor, SiteExtractor
 from .spglib import SpglibCell
 
-__all__ = ["cell_from_input"]
+__all__ = ["cell_from_input", "cell_to_input"]
 
 
 def cell_from_input(incar, poscar):
@@ -14,3 +16,16 @@ def cell_from_input(incar, poscar):
     positions = [site.frac_coords for site in SiteExtractor.from_file(poscar)]
     atoms = AtomsExtractor.from_file(poscar)
     return SpglibCell(lattice, positions, atoms, magmoms)
+
+
+def cell_to_input(cell, incar, poscar):
+    """Write a cell object to INCAR and POSCAR files."""
+    incar, poscar = Path(incar), Path(poscar)
+    if not incar.exists() or not poscar.exists():
+        incar.touch(exist_ok=True)
+        poscar.touch(exist_ok=True)
+    incar_data = Incar.from_file(incar)
+    if cell.magmoms is not None:
+        incar_data["MAGMOM"] = cell.magmoms
+    incar_data.write_file(incar)
+    Poscar(cell.to_structure()).write_file(poscar)
