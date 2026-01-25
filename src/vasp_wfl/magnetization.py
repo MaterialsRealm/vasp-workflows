5c812d3f49762082939adcfe21e014c03292276e
singularitti <singularitti@outlook.com>
2026-01-25 00:00:57 -0600

    Add `from_dir` to `MagnetizationParser`

--- src/vasp_wfl/magnetization.py
+++ src/vasp_wfl/magnetization.py
@@ -1,6 +1,8 @@
 from pandas import DataFrame
 from pymatgen.io.vasp import Oszicar, Outcar
 
+from .workdir import Workdir
+
 __all__ = ["MagnetizationParser"]
 
 
@@ -22,4 +24,31 @@ class MagnetizationParser:
             oszicar = Oszicar(file)
             return DataFrame(oszicar.ionic_steps).mag
         except Exception:
+            return None
+
+    @staticmethod
+    def from_dir(workdir: Workdir):
+        """Return magnetization information from a `Workdir`.
+
+        Check for `OUTCAR` first and parse it with :meth:`from_outcar`.
+        If `OUTCAR` is missing, try `OSZICAR` with :meth:`from_oszicar`.
+
+        Args:
+            workdir: A :class:`Workdir` instance or path-like pointing to a folder.
+
+        Returns:
+            A :class:`pandas.DataFrame` or pandas Series with magnetization data, or
+            ``None`` if no suitable file was found or parsing failed.
+        """
+        try:
+            # accept either Workdir or path-like input
+            root = workdir.path
+            outcar_file = root / "OUTCAR"
+            if outcar_file.exists():
+                return MagnetizationParser.from_outcar(outcar_file)
+            oszicar_file = root / "OSZICAR"
+            if oszicar_file.exists():
+                return MagnetizationParser.from_oszicar(oszicar_file)
+            return None
+        except FileNotFoundError:
             return None
