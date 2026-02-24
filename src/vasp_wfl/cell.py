e10c9ecc9a9f898ce6c75689c51ffb2023cf9953
singularitti <singularitti@outlook.com>
2026-02-23 19:02:00 -0600

    Deprecate `count_elements` in src/vasp_wfl/cell.py
    We already have `ElementCounter`

--- src/vasp_wfl/cell.py
+++ src/vasp_wfl/cell.py
@@ -1,9 +1,6 @@
-from collections import Counter
-
-from pymatgen.core.periodic_table import Element
 from pymatgen.io.vasp import Oszicar, Poscar
 
-__all__ = ["count_elements", "get_cell", "get_energies", "get_volume"]
+__all__ = ["get_cell", "get_energies", "get_volume"]
 
 
 def get_cell(filename):
@@ -16,16 +13,6 @@ def get_volume(filename):
     """Get the volume of the cell from a VASP POSCAR file."""
     poscar = Poscar.from_file(filename)
     return poscar.structure.volume
-
-
-def count_elements(filename):
-    """Count the number of atoms for each element in a VASP POSCAR file."""
-    poscar = Poscar.from_file(filename)
-    atomic_numbers = poscar.structure.atomic_numbers
-    # Convert atomic numbers to element symbols
-    symbols = [Element.from_Z(z).symbol for z in atomic_numbers]
-    counts = dict(Counter(symbols))
-    return counts
 
 
 def get_energies(filename):
